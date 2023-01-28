0632ade8e72eb3801fbbd345519af0a1e7bbd1f6
motsu8 <motsu159357@gmail.com>
2023-01-28 22:05:43 +0900

    conflict 解消

--- game/src/components/Square.jsx
+++ game/src/components/Square.jsx
@@ -1,9 +1,8 @@
 import React from "react";
 import "./Square.css";
 
-const Square = ({ item, winnerSquare, squareValue, onClick }) => {
-	console.log(winnerSquare);
-	let square = "square " + winnerSquare
+const Square = ({item, winnerSquare, squareValue, onClick}) => {
+	let square = "square " + winnerSquare;
 	return (
       <button
 				className={square}
